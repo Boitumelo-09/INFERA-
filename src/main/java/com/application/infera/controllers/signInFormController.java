d3f5d945eb20930e54773f54adbf3d795075cb16
boitumelo-09 <belmiredub4@gmail.com>
2026-07-05 18:50:45 +0200

    Revert "Add sign-in functionality with server-side validation and error handling"
    This reverts commit b62839faf16d083333dd2da853ba132166ef91de.

--- src/main/java/com/application/infera/controllers/signInFormController.java
+++ src/main/java/com/application/infera/controllers/signInFormController.java
@@ -1,48 +1,12 @@
 package com.application.infera.controllers;
 
-import com.application.infera.dtos.requests.SignInRequest;
-import com.application.infera.exception.InvalidUserCredentialException;
-import com.application.infera.exception.UserDoesntExistByEmailException;
-import com.application.infera.models.User;
-import com.application.infera.services.UserService;
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class signInFormController {
-
-    private final UserService userService;
-
-    public signInFormController(UserService userService) {
-
-        this.userService = userService;
-    }
-
     @GetMapping("/signin")
-    public String signInForm(Model model) {
-        System.out.println("Sign In Form - CALLED");
-        model.addAttribute("signInRequest", new SignInRequest());
+    public String signInForm() {
         return "signin";
-    }
-    @PostMapping("/signin")
-    public String signIn(@Valid @ModelAttribute SignInRequest signInRequest, BindingResult bd, Model model) {
-        System.out.println("Sign In - CALLED");
-        if (bd.hasErrors()) {
-            System.out.println("Binding Result has Errors");
-            return "signin";
-        }
-        try {
-           User user = userService.signIn(signInRequest);
-        } catch (UserDoesntExistByEmailException | InvalidUserCredentialException e) {
-            model.addAttribute("errorCredMessage", e.getMessage());
-            return "signin";
-        }
-
-        return "redirect:/dashboard";
     }
 }
--- src/main/java/com/application/infera/exception/UserDoesntExistByEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.application.infera.exception;
-
-public class UserDoesntExistByEmailException extends RuntimeException{
-    public UserDoesntExistByEmailException(String message) {
-        super(message);
-    }
-}
