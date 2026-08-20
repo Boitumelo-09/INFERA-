9f69fcb5adde10f96dde084492fd72116115f80e
boitumelo-09 <belmiredub4@gmail.com>
2026-08-20 15:19:39 +0200

    update activity and dashboard card styling for hover effects and layout adjustments

---
+++ src/main/java/com/application/infera/dtos/requests/EmailRequest.java
@@ -0,0 +1,12 @@
+package com.application.infera.dtos.requests;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class EmailRequest {
+    @NotBlank
+    @Email
+    private String email;
+}
