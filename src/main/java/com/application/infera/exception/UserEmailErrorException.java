95a0cce7bce24ac86629880b2b68c469f6987d80
boitumelo-09 <belmiredub4@gmail.com>
2026-08-21 16:00:33 +0200

    update activity and dashboard card styling for hover effects and layout adjustments

--- src/main/java/com/application/infera/exception/UserEmailErrorException.java
+++
@@ -1,7 +0,0 @@
-package com.application.infera.exception;
-
-public class UserEmailErrorException extends RuntimeException{
-    public UserEmailErrorException(String message) {
-        super(message);
-    }
-}
