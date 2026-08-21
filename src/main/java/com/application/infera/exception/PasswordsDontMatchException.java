c2c8a718b2102b1763aad0c014bb0b6178cc55aa
boitumelo-09 <belmiredub4@gmail.com>
2026-08-21 16:01:48 +0200

    update activity and dashboard card styling for hover effects and layout adjustments

--- src/main/java/com/application/infera/exception/PasswordsDontMatchException.java
+++
@@ -1,7 +0,0 @@
-package com.application.infera.exception;
-
-public class PasswordsDontMatchException extends RuntimeException{
-    public PasswordsDontMatchException(String message) {
-        super(message);
-    }
-}
