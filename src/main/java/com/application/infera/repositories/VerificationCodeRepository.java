78f6dc106871fa1d2c69d5d5a5186d72c8319127
boitumelo-09 <belmiredub4@gmail.com>
2026-08-21 16:00:17 +0200

    update activity and dashboard card styling for hover effects and layout adjustments

--- src/main/java/com/application/infera/repositories/VerificationCodeRepository.java
+++ src/main/java/com/application/infera/repositories/VerificationCodeRepository.java
@@ -3,9 +3,11 @@ package com.application.infera.repositories;
 import com.application.infera.models.VerificationCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
     Optional<VerificationCode> findTopByEmailOrderByCreatedAtDesc(String email);
     void deleteByEmail(String email);
+    void deleteByExpiresAtBefore(LocalDateTime cutoff);
 }
